1de0df36b826201b0c27273b9e2e110a8828d997
ivan91simeonov <vanches91@abv.bg>
2022-06-27 22:04:21 +0300

    PhoneCongfiration

--- src/pages/PhoneConfirmation/PhoneConfirmation.jsx
+++ src/pages/PhoneConfirmation/PhoneConfirmation.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
 import './PhoneConfirmation.css';
 import { Link } from 'react-router-dom';
+import PhoneInput from 'react-phone-number-input';
 
 function PhoneConfirmation() {
   const [value, setValue] = useState();
 
-  return <div className='phone-confirm-container'>
-        <h1>Enter your phone</h1>
-  </div>;
+  return (
+    <div className="phone-confirm-container">
+      <Link to='/' className='back-btn'></Link>
+      <h1>Enter your phone</h1>
+      <PhoneInput international defaultCountry="BG" value={value} />
+      <p>
+        By entering your number , you're agreeing to our {''}
+        <span>Terms of Service and Privacy Policy. </span>
+        Thanks!
+      </p>
+    </div>
+  );
 }
 
 export default PhoneConfirmation;
